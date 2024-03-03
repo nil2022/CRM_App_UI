9b13c7a37c3ba87b91890a63def15405c937c187
Nilanjan Haldar <nil.haldar@gmail.com>
2024-03-03 14:30:53 +0530

    fix: signup & signin logic, toast

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,105 +1,134 @@
 import { ArrowRight } from 'lucide-react'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import axios from 'axios'
-import toast,{ Toaster } from 'react-hot-toast'
-import { Link } from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast'
+import { Link, redirect } from 'react-router-dom'
 
 function Signin() {
-    // console.log("login:", login)
+    
+    /** set success message when success is true */
+    const [successMsg, setSuccessMsg] = useState({})
+    /** set error message when error is true */
+    const [errorMsg, setErrorMsg] = useState({})
+    /** set loading state  */
+    const [loading, setLoading] = useState(false)
+    const intialValues = { userId: "", password: "" };
+    const [formValues, setFormValues] = useState(intialValues);
 
-    const [data, setData] = useState({})
-    const [userId, setUserId] = useState("")
-    const [password, setPassword] = useState("")
-  
-    const login = useCallback(async() => {
-      await axios.post(`${import.meta.env.VITE_CRM_BACKEND_URL}/crm/api/auth/signin`,
-      {
-        userId,
-        password
-      })
-      .then((res) => {
-        setData(res.data)
-        console.log(res.data)
-        toast.success(res.data?.message || data?.message,
-            {
-                duration: 3000
-            })
-      })
-      .catch((err) => {
-        setData(err.response)
-        console.log("Got Error:",err.response)
-        // console.log({
-        //     message:'error in cloud',
-        //     error1: err.response.data?.message,
-        //     error2: err?.message,
-        //     error3: err.data?.error?.message
-        // })
-        toast.error(err.response.data?.message || err?.message || err.data?.error?.message,
-            {
-                duration: 3000
-            })
-      })
-    },[userId, password, data])
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setFormValues({ ...formValues, [id]: value });
+        console.log(formValues);
+    }
+
+//    function handleSubmit(e) {
+//        e.preventDefault()
+//        console.log('submit button pressed')
+//        setLoading(true)
+//        setTimeout(() => {
+//         console.log('executed after 3 seconds')
+//         setLoading(false)
+//         toast.dismiss()
+//         // toast.success('Hii this is toast')
+//         toast(`userid: ${formValues.userId} password: ${formValues.password}`)
+//        }, 3000);
+//        console.log('userid:', formValues.userId, 'password:', formValues.password)
+       
+//    }
+
+    const handleLogin = useCallback(async(e) => {
+        e.preventDefault()
+        
+        const signInUrl = import.meta.env.VITE_CRM_BACKEND_URL
+        setLoading(true)
+        try {
+            const response = await axios.post(`${signInUrl}/crm/api/auth/signin`,
+                {
+                    userId: formValues.userId,
+                    password: formValues.password
+                })
+
+            setLoading(false)
+            toast.dismiss()
+            setSuccessMsg(response.data)
+            console.log('success:', response.data)
+            toast.success(response.data.message)
+        } catch (error) {
+            setLoading(false)
+            toast.dismiss()
+            setErrorMsg(error)
+            console.log("Got Error:", error)
+            error.response?.data.message ?  toast.error(error.response.data.message) : toast.error(error.message)
+        }
+    }, [formValues])
+
+
+    useEffect(() => {
+
+        if (loading) {
+            toast.loading('Please wait.....')
+        }
+
+        return () => {
+            toast.dismiss()
+        }
+    }, [loading])
+
     return (
         <>
+            <div><Toaster position="top-center" /></div>
             <section>
                 <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
-                    <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+                    <div className="sm:mx-auto sm:w-full sm:max-w-sm w-[80%]">
                         <div className="mb-2 flex justify-center">
-                            <svg
-                                width="50"
-                                height="56"
-                                viewBox="0 0 50 56"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
-                                    fill="black"
-                                />
-                            </svg>
+                            <img src="../../src/assets/icons8-login-100.png"
+                                className="h-12 w-12"
+                                alt="login-image" />
                         </div>
                         <h2 className="text-center text-2xl font-bold leading-tight text-black">
-                            Sign in to your account
+                            Welcome back !
                         </h2>
                         <p className="mt-2 text-center text-base text-gray-600">
                             Don&apos;t have an account?{' '}
-                            <br/>
+                            <br />
                             <Link
                                 to="/signup"
+                                rel="noopener noreferrer"
                                 className="font-[800] text-black transition-all duration-200 hover:underline"
                             >
                                 Register Now !
                             </Link>
                         </p>
-                        <form action="#" 
-                            method="POST" 
+                        <form
+                            onSubmit={handleLogin}
                             className="mt-8"
-                            onSubmit={(e) => e.preventDefault()}>
+                        >
                             <div className="space-y-5">
                                 <div>
                                     <div className="flex items-center justify-between">
-                                        <label htmlFor="userId" 
-                                        className="text-base font-[800] text-gray-900">
-                                        {' '}
-                                        UserID{' '}
-                                    </label>
-                                    </div>                                
+                                        <label htmlFor="userId"
+                                            className="text-base font-[500] text-gray-900">
+                                            {' '}
+                                            Username{' '}
+                                        </label>
+                                    </div>
                                     <div className="mt-2">
                                         <input
                                             className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                                             type="text"
-                                            autoComplete="userId"
-                                            placeholder="UserID"
+                                            autoComplete='on'
+                                            placeholder="John Doe"
                                             id="userId"
-                                            onChange={(e) => setUserId(e.target.value)}
-                                        ></input>
+                                            value={formValues.userId}
+                                            onChange={handleChange}
+                                            required
+                                        />
                                     </div>
                                 </div>
                                 <div>
                                     <div className="flex items-center justify-between">
-                                        <label htmlFor="password" 
-                                        className="text-base font-[800] text-gray-900">
+                                        <label htmlFor="password"
+                                            className="text-base font-[500] text-gray-900">
                                             {' '}
                                             Password{' '}
                                         </label>
@@ -108,17 +137,18 @@ function Signin() {
                                         <input
                                             className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                                             type="password"
+                                            autoComplete='on'
                                             placeholder="Password"
-                                            autoComplete="password"
                                             id="password"
-                                            onChange={(e) => setPassword(e.target.value)}
-                                        ></input>
+                                            value={formValues.password}
+                                            onChange={handleChange}
+                                            required
+                                        />
                                     </div>
                                 </div>
                                 <div>
                                     <button
-                                    onClick={login}
-                                        type="button"
+                                        // onClick={handleLogin}
                                         className="inline-flex w-full items-center justify-center rounded-lg bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-slate-700 transition-all duration-400"
                                     >
                                         Sign In <ArrowRight className="ml-2" size={16} />
@@ -177,7 +207,6 @@ function Signin() {
                     button, it may take time because the backend of this app is connected with a server which sleeps after inactivity.
                 </div> */}
             </section>
-            <Toaster position="top-center" />
         </>
     )
 }
