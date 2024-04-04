61ce8a288d000e6309f909287264728c92cf8bc6
Nilanjan Haldar <nil.haldar@gmail.com>
2024-04-04 17:03:35 +0530

    update:footer-UI-changes, code changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { GitHub, LinkedIn } from '@mui/icons-material';
+import { GitHub, LinkedIn, CopyrightRounded } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
-import { CopyrightIcon } from 'lucide-react';
 
 export function Footer() {
   return (
@@ -29,24 +28,29 @@ export function Footer() {
             </div>
           </li>
 
-          <ul className="flex flex-col max-w-[500px] sm:flex-row p-3 text-left sm:text-center gap-y-1  ">
+          <ul className="flex flex-col min-w-fit sm:flex-row p-3 sm:items-center text-left sm:text-center gap-y-1 gap-x-2 ">
 
-            <li className="min-w-[100px] sm:p-2 text-left sm:text-center hover:bg-white rounded-md hover:scale-105 sm:rounded-xl transition-all duration-500">
-              <Link className="text-lg text-gray-800 hover:text-gray-700 hover:underline" to="#">
+            <li className="min-w-fit sm:px-2 text-left sm:text-center hover:bg-white rounded-md hover:scale-105 sm:rounded-xl transition-all duration-500">
+              <Link className="md:text-lg text-gray-800 hover:text-gray-700 hover:underline" to="#">
                 Privacy Policy
               </Link>
             </li>
-            <li className="min-w-[100px] sm:p-2 text-left sm:text-center hover:bg-white rounded-md hover:scale-105 sm:rounded-xl transition-all duration-500 ">
-              <Link className="text-lg text-gray-800 hover:text-gray-700 hover:underline" to="https://www.linkedin.com/in/nil2022" target='_blank'>
+            <li className="min-w-fit sm:px-2 text-left sm:text-center hover:bg-white rounded-md hover:scale-105 sm:rounded-xl transition-all duration-500 ">
+              <Link className="md:text-lg text-gray-800 hover:text-gray-700 hover:underline" to="https://www.linkedin.com/in/nil2022" target='_blank'>
                 Contact Me
               </Link>
             </li>
           </ul>
 
-          <div className="flex flex-col sm:flex-row p-3 min-w-[200px] sm:max-w-[500px] items-center justify-center gap-x-1 bg-violet-700 text-white font-[300] sm:text-black sm:bg-transparent ">          
-            <span className='flex items-center text-center justify-center'><CopyrightIcon size={25} className='text-white sm:text-gray-800 p-1' />
-            {' '}Copyright 2024. All Rights Reserved. Created by </span>
-            <a href="https://github.com/nil2022" target='_blank' className='hover:text-gray-700 hover:underline font-[400]'>Nilanjan</a>
+          <div className="flex flex-col sm:flex-row p-3 min-w-[200px] sm:max-w-[500px] items-center justify-center bg-violet-700 text-white font-[300] sm:text-black sm:bg-transparent ">
+            <span className='flex items-center justify-center text-sm '>
+              <CopyrightRounded sx={{ fontSize: 20, marginX: 1 }} className='text-white sm:text-gray-800' />
+              {' '}Copyright 2024. All Rights Reserved. Created by
+            </span>
+            <a href="https://github.com/nil2022" target='_blank' className='hover:text-gray-700 hover:underline font-[400] px-2'
+            >
+              Nilanjan
+            </a>
           </div>
         </ul>
       </div>
